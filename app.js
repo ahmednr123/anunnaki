8781c0f800a9eec14dab0bd823ca7a8c91871875
Ahmed Noor <ahmed@ahmed>
2018-10-13 20:34:16 +0500

    / route added

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const port = process.env.PORT || 8080
 
 app.use(express.static('public'))
 
+app.get('/', (req, res) => {
+
+	res.end('[LIB] - http://lib.qett.in/mvc/vanilla.js (v1.0)')
+
+})
+
 app.listen(port, function(){
 	console.log('Serving js libraries to save the world!')
 })
